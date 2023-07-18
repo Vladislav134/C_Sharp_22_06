705716b105451a812d58c9e9347ec3e0d4acbf72
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-07-19 01:54:27 +0300

    IS OK

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine( "Введите два числа: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+ if ( a < b )
+ {
+    Console.WriteLine("{0} наибольшее число", b);
+ }
+ else 
+ {
+    Console.WriteLine("{0} наибольшее число", a);
+ }
+ Console.ReadLine();
