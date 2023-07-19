715deef1836ead82639cad89c69a0dbcc30a3c62
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-07-19 04:19:36 +0300

    Mission complited

--- HW1/Zadacha3/Program.cs
+++ HW1/Zadacha3/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("введите новое число");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
