f5587a10c7d1b0c8501d1f8efc5cb837fd63084f
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-07-19 05:14:22 +0300

    mission complited

---
+++ HW1/Zadacha4/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("введите любое число");
+int a = int.Parse(Console.ReadLine()!);
+int count = 1;
+while (count <= a)
+{
+    if 
+        (count % 2 == 0)
+    Console.WriteLine(count);
+     count++;
+}
