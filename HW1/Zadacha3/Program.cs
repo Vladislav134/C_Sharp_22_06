76d4ad8ddf11ecb27290a97e4c0545d574ebdbd2
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-07-20 07:44:21 +0300

    New

--- HW1/Zadacha3/Program.cs
+++ HW1/Zadacha3/Program.cs
@@ -1,12 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("введите новое число");
+Console.WriteLine("введите любое число");
 int a = int.Parse(Console.ReadLine()!);
-if (a % 2 == 0)
+int count = 1;
+while (count <= a)
 {
-    Console.WriteLine("Число четное");
+    if 
+        (count % 2 == 0)
+    Console.WriteLine(count);
+     count++;
 }
-else
-{
-    Console.WriteLine("Число нечетное");
-}
+   
+
+
