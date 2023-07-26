8bd5d7bb8e56848082112d38f17f31410a9308df
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-07-26 16:46:45 +0300

    Est' contact

--- 2_1/Program.cs
+++ 2_1/Program.cs
@@ -1,11 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 int TakeNum(int num)
+{       
+if (num < 100)
+     {
+Console.WriteLine("третьей цифры нет");
+     }
+if (num >= 100)
+     {
+while (num >= 1000)
 {
-    Console.WriteLine("" + num);
-    return num / 10 % 10;
-        }
-    Console.WriteLine("Введите любое трехзначное число");
-    int num2 = int.Parse(Console.ReadLine());
-    int result = TakeNum(num2);
+        num = num / 10;
+                    }
+        num = num % 10; }   
+        return num;
+}
+
+    Console.WriteLine("Введите любое число");
+    int num3 = int.Parse(Console.ReadLine()!);
+    int result = TakeNum(num3);
     Console.WriteLine(result);
