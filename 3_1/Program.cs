70a9f546fb6d568a608444824619476186e68e2d
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-08-01 18:28:44 +0300

    3_1

--- 3_1/Program.cs
+++ 3_1/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Введите пятизначное число");
 int a = int.Parse(Console.ReadLine()!);
 if (a > 99999 || a < 10000)
-Console.WriteLine("Введено не пятизначное число");
+Console.WriteLine(" Введено не пятизначное число ");
 else if (a / 10000 == a % 10 && a / 1000 % 10 == a % 100 / 10)
 Console.WriteLine("Число является паллиндромом");
 else 
