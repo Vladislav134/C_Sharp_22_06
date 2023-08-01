d192794a69f2fe1f92017a795cb876dc0239da60
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-08-01 18:26:06 +0300

    3_2

--- 3_1/Program.cs
+++ 3_1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine()!);
+if (a > 99999 || a < 10000)
+Console.WriteLine("Введено не пятизначное число");
+else if (a / 10000 == a % 10 && a / 1000 % 10 == a % 100 / 10)
+Console.WriteLine("Число является паллиндромом");
+else 
+Console.WriteLine("Число не является паллиндромом");
