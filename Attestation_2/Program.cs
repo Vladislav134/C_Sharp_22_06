2b07ece1955d689ab3cc548b698772112529df8e
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-08-03 22:30:33 +0300

    URA2

---
+++ Attestation_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int Sum(int n, int m)
+{
+    
+    if (n <= 0 && m <= 0)
+    Console.WriteLine("числа не натуральные");
+    if (n < m)
+    return 0;
+     return Sum(n - 1, m) + n;
+    } 
+    Console.Write($" {Sum(8 , 3)} ");
