44d4f004a9862b17f8838d936febb55853da0caa
Vladislav134 <137281737+Vladislav134@users.noreply.github.com>
2023-08-03 19:54:32 +0300

    URA1

--- Attestation/Program.cs
+++ Attestation/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 Console.WriteLine("Hello, World!");
+void PrintNumber(int n, int m)
+{
+    if (n % 2 == 1)
+    n = n - 1;
+    if (n < m)
+    return;
+  PrintNumber(n-1, m);
+Console.Write($" {n} ");
+}
+PrintNumber(11, 3);
